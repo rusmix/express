7c3736377195db8342f5613c72dc0ac6881b3031
StasProfile <61944126+StasProfile@users.noreply.github.com>
2022-07-18 14:12:50 +0300

    added get /users pagination

--- routes/users.js
+++ routes/users.js
@@ -1,29 +1,36 @@
-const express = require('express');
+const express = require("express");
 const usersRouter = express.Router();
-const UsersController = require('../controllers/usersController');
-const { body } = require('express-validator');
-const User = require('../database/models/user');
+const UsersController = require("../controllers/usersController");
+const { body, query } = require("express-validator");
+const User = require("../database/models/user");
 
-const usersController = new UsersController(User)
+const usersController = new UsersController(User);
 
-usersRouter.get('/', usersController.getUsers);
-
-usersRouter.get('/:id', usersController.getUser);
-
-usersRouter.post('/',
-    body('name').isString(),
-    body('login').isString(),
-    body('password').isString(),
-    usersController.createUser
+usersRouter.get(
+  "/",
+  query("limit").isInt({ gt: 0 }),
+  query("page").isInt({ gt: 0 }),
+  usersController.getUsers
 );
 
-usersRouter.delete('/:id', usersController.deleteUser);
+usersRouter.get("/:id", usersController.getUser);
 
-usersRouter.patch('/:id',
-    body('name').if(body('name').exists()).isString(),
-    body('login').if(body('login').exists()).isString(),
-    body('password').if(body('password').exists()).isString(),
-    usersController.updateUser
+usersRouter.post(
+  "/",
+  body("name").isString(),
+  body("login").isString(),
+  body("password").isString(),
+  usersController.createUser
+);
+
+usersRouter.delete("/:id", usersController.deleteUser);
+
+usersRouter.patch(
+  "/:id",
+  body("name").if(body("name").exists()).isString(),
+  body("login").if(body("login").exists()).isString(),
+  body("password").if(body("password").exists()).isString(),
+  usersController.updateUser
 );
 
 module.exports = usersRouter;
